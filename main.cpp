4eb935eee5f9441af82d9731745abba11b40bb13
hly920928 <hly920928@gmail.com>
2018-01-08 00:14:15 +0800

    Simple_Console_IO_Echo

--- main.cpp
+++ main.cpp
@@ -2,11 +2,26 @@
 #include "myHeader.h"
 #include <iostream>
 int main(int argc, char* argv[]) {
-	char pwdBuffer[999];
-	DWORD lenCurDir;
-	lenCurDir=GetCurrentDirectory(999,pwdBuffer);
-    PrintMsg(GetStdHandle(STD_OUTPUT_HANDLE),pwdBuffer);
-	return 0;
+   HANDLE hIn,hOut;
+   DWORD count;
+   hIn=CreateFile(_T("CONIN$"),
+   GENERIC_READ|GENERIC_WRITE,0,
+   NULL,OPEN_ALWAYS,FILE_ATTRIBUTE_NORMAL,NULL);
+   hOut=CreateFile(_T("CONOUT$"),
+   GENERIC_WRITE,0,
+   NULL,OPEN_ALWAYS,FILE_ATTRIBUTE_NORMAL,NULL);
+   char pBuffer[999];
+   const char str[]="Please InPut\n";
+   WriteConsole(hOut,str,strlen(str),&count,NULL);
+   while(true){
+   	ReadConsole(hIn,pBuffer,999,&count,NULL);
+   	pBuffer[count-2]='\n';pBuffer[count-1]='\0';
+   	if(pBuffer[0]=='0')break;
+   	WriteConsole(hOut,pBuffer,count-1,&count,NULL);
+   }
+   const char end[]="Echo End\n";
+   WriteConsole(hOut,end,strlen(end),&count,NULL);
+    return 0;
 }
 
 
