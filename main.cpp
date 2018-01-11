45be0dd37eff16cd14de7fe241d1931cfa8f9490
hly920928 <hly920928@gmail.com>
2018-01-11 19:36:00 +0800

    Change_File_Time

--- main.cpp
+++ main.cpp
@@ -3,10 +3,17 @@
 #include <iostream>
 
 int main(int argc, char* argv[]) {
-    LPCTSTR filename="123456.txt";
-    LPCTSTR Dirname="*";
-    BOOL flag[10];flag[0]=1;
-    TraverseDirectory(filename,Dirname,666,flag);
+    FILETIME newFileTime;
+	LPFILETIME pAccessTime = NULL, pModifyTime = NULL;
+    HANDLE hFile;
+    const char fileName[]="123456.txt\0";
+    hFile = CreateFile(fileName, GENERIC_READ | GENERIC_WRITE, 0, NULL,
+				OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+    GetSystemTimeAsFileTime(&newFileTime);
+    pAccessTime = &newFileTime;
+    pModifyTime = &newFileTime;
+    SetFileTime(hFile, NULL, pAccessTime, pModifyTime);
+    CloseHandle(hFile);
     return 0;
 }
 
